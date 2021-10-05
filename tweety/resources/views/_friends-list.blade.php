03586290e6f4f90e15727c871a993caa8f54bb9a
Sergio Sanchez <sergiosanchezv20@gmail.com>
2021-10-05 17:29:52 -0300

    avances hasta ep 56

---
+++ tweety/resources/views/_friends-list.blade.php
@@ -0,0 +1,16 @@
+        <h3 class="font-bold text-xl mb-4">Friends</h3>
+        
+        <ul>
+			<li>
+                <div class="flex items-center">
+                    <img
+                        src="https://i.pravatar.cc/40"
+                        alt=""
+                        class="rounded-full mr-2"
+                    >   
+
+                John Doe
+            <li>
+		</ul>
+		
+
